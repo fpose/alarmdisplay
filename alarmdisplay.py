91dca80f184458b2f0c32cc9bebe2d8affa09a25
Florian Pose <florian@pose.nrw>
2017-12-20 16:09:41 +0100

    Show maximized.

--- alarmdisplay.py
+++ alarmdisplay.py
@@ -11,6 +11,6 @@ if __name__ == '__main__':
     app = PyQt5.QtWidgets.QApplication(sys.argv)
 
     mainWidget = MainWidget.MainWidget()
-    mainWidget.show()
+    mainWidget.showMaximized()
 
     sys.exit(app.exec_())
