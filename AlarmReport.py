f21cc1b6873fc8a1099a33bb6a0cdac3144918be
Florian Pose <florian@pose.nrw>
2017-12-29 16:53:37 +0100

    Generate template; run LaTeX.

--- AlarmReport.py
+++ AlarmReport.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 
+import os
+import codecs
+import tempfile
+from Cheetah.Template import Template
+import subprocess
+import shutil
+
 import Map
 
 class AlarmReport:
@@ -12,13 +19,91 @@ class AlarmReport:
         self.map_width = res * 120
         self.map_height = res * 120
 
+        # Load Template
+        templateDir = self.config.get("report", "template_dir",
+                fallback = "report")
+        templatePath = os.path.join(templateDir, 'template.tex')
+        templateFile = codecs.open(templatePath, encoding="utf-8", mode = 'r')
+        self.template = templateFile.read()
+        self.logger.debug(repr(self.template))
+        templateFile.close()
+
     def generate(self, lat_deg, lon_deg, route):
+
+        tempDir = tempfile.mkdtemp(prefix = 'alarm-')
+
         targetPixmap = Map.getTargetPixmap(lat_deg, lon_deg,
                 self.map_width, self.map_height,
                 route[0], self.config, self.logger)
-        targetPixmap.save('/tmp/target.png')
+        targetPixmap.save(os.path.join(tempDir, 'target.png'))
 
         routePixmap, markerRects = Map.getRoutePixmap(lat_deg, lon_deg,
                 self.map_width, self.map_height,
                 route[0], self.config, self.logger)
-        routePixmap.save('/tmp/route.png')
+        routePixmap.save(os.path.join(tempDir, 'route.png'))
+
+        variables = {}
+        # TODO fill
+
+        try:
+            templateOutput = Template(self.template, searchList = variables)
+        except:
+            self.logger.error('Failed to process template', exc_info = True)
+            return
+
+        texBase = 'alarm'
+        texPath = os.path.join(tempDir, texBase) + '.tex'
+        self.logger.info(u'Creating LaTeX file %s', texPath)
+
+        outFile = codecs.open(texPath, encoding='utf-8', mode = 'w')
+        outFile.write(str(templateOutput))
+        outFile.close()
+
+        self.logger.info(u'Starting LaTeX processing...')
+
+        templateDir = self.config.get("report", "template_dir",
+                fallback = "report")
+        imageDir = self.config.get("display", "image_dir",
+                fallback = "images")
+        inputPaths = [templateDir, imageDir]
+        inputs = '.:'
+        for path in inputPaths:
+            inputs += ':' + path
+        latexEnv = os.environ
+        self.logger.info(u'TEXINPUTS=%s', inputs)
+        latexEnv['TEXINPUTS'] = inputs
+
+        devNull = open(os.devnull, 'w')
+
+        cmd = ['pdflatex', '-interaction=batchmode', texPath]
+        self.logger.info(u'Running %s', cmd)
+        latex = subprocess.Popen(cmd, cwd = tempDir, stdout = devNull,
+                env = latexEnv)
+        latex.wait()
+        devNull.close()
+
+        if latex.returncode != 0:
+            logPath = os.path.join(tempDir, texBase) + '.log'
+            self.logger.error(u'LaTeX processing failed; see log in %s',
+                    logPath)
+            return
+
+        self.logger.info(u'Copying PDF file...')
+
+        targetDir = '.' # TODO
+        pdfPath = os.path.join(tempDir, texBase + '.pdf')
+        pdfTarget = os.path.join(targetDir, texBase + '.pdf')
+        shutil.copy(pdfPath, pdfTarget)
+
+        self.logger.info(u'Deleting temporary directory...')
+        shutil.rmtree(tempDir)
+
+        self.logger.info(u'PDF file %s was created.', pdfTarget)
+
+        printImmediately = False # TODO
+        if printImmediately:
+            self.logger.info("Printing PDF file.")
+            lpr = subprocess.Popen(['lpr', pdfTarget])
+            lpr.wait()
+            if lpr.returncode != 0:
+                self.logger.error('lpr failed.')
