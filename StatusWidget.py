428cf9710f2f2ba45648a2c23ae7ff6ef0f1cfb8
Florian Pose <florian@pose.nrw>
2021-02-11 11:47:35 +0100

    Status labels and animations.

--- StatusWidget.py
+++ StatusWidget.py
@@ -30,6 +30,7 @@ import re
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
+from PyQt5.QtSvg import QSvgRenderer
 
 #-----------------------------------------------------------------------------
 
@@ -43,14 +44,29 @@ class StatusWidget(QWidget):
         self.logger = main.logger
         websocketReceiver = main.websocketReceiver
 
-        self.imageDir = self.config.get("display", "image_dir",
+        imageDir = self.config.get("display", "image_dir",
                 fallback = "images")
+        self.statusPixmaps = {}
+        for s in (1, 2, 3, 4, 6):
+            path = os.path.join(imageDir, 'status%u.svg' % (s))
+            try:
+                renderer = QSvgRenderer(path)
+                pixmap = QPixmap(48, 48);
+                pixmap.fill(Qt.transparent);
+                painter = QPainter(pixmap);
+                renderer.render(painter, QRectF(pixmap.rect()));
+            except:
+                self.logger.warning('Failed to load status image %s.', path,
+                        exc_info = True)
+                continue
+            self.statusPixmaps[s] = pixmap
 
         layout = QHBoxLayout(self)
         layout.setSpacing(0)
         layout.setContentsMargins(0, 0, 0, 0)
 
         self.labelDict = {}
+        self.animationDict = {}
         for address, name in websocketReceiver.status:
             label = QLabel(self)
             layout.addWidget(label, 1)
@@ -61,20 +77,53 @@ class StatusWidget(QWidget):
                 """)
             label.setText(name)
 
-            label = QLabel(self)
+            label = AnimatedLabel(self)
             layout.addWidget(label, 0)
-            label.setStyleSheet("""
-                font-size: 40px;
-                padding: 10px;
-                """)
             self.labelDict[address] = label
+
+            animation = QPropertyAnimation(label, b'color')
+            animation.setStartValue(QColor(Qt.transparent))
+            animation.setKeyValueAt(0.5, QColor(Qt.white))
+            animation.setEndValue(QColor(Qt.transparent))
+            animation.setDuration(1000)
+            animation.setLoopCount(10)
+            animation.setEasingCurve(QEasingCurve.OutInQuad);
+            self.animationDict[address] = animation
 
         label = QLabel(self)
         layout.addWidget(label, 1)
 
     def setStatus(self, status):
-        if status['address'] in self.labelDict:
-            label = self.labelDict[status['address']]
-            label.setText(str(status['status']))
+        address = status['address']
+        if address in self.labelDict:
+            label = self.labelDict[address]
+            s = status['status']
+            if s in self.statusPixmaps:
+                label.setPixmap(self.statusPixmaps[s])
+                label.setText('')
+            else:
+                label.setPixmap(QPixmap())
+                label.setText(str(s))
+        if address in self.animationDict:
+            self.animationDict[address].start()
+
+#-----------------------------------------------------------------------------
+
+class AnimatedLabel(QLabel):
+
+    def __init__(self, parent):
+        super(AnimatedLabel, self).__init__(parent)
+
+    def setColor(self, color):
+        s = """
+        font-size: 40px;
+        padding: 10px;
+        background-color: #%08x;""" % (color.rgba())
+        self.setStyleSheet(s)
+
+    def getColor(self):
+        return Qt.black
+
+    color = pyqtProperty(QColor, getColor, setColor)
 
 #-----------------------------------------------------------------------------
